8ba4fa3c3b1698ad5ef48535c95f5cba440c54dd
Matthew Dawson <matthewdawson@outlook.com>
2020-01-02 22:43:34 +1300

    Fix hover over list shadow bug
    This was caused by the target offset being reset to 0 when hovering over
    the ul element. Using a CSS grid when would only add the offset of the
    grid divs.
    
    This commit adds the offset of the ul element if the target is withen
    the list.
    
    This problem may be encountered later if more elements are added within
    the individual grid element. It is not worth comming up with a
    encompassing solution until we run into this again.
    
    A more permanment fix may be to check if the target is within a element
    that is nested within another element, then add the offset.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 const body = document.querySelector('body');
+const list = document.querySelector('ul');
 const walk = 50;
 
 function shadow(e){
@@ -9,6 +10,11 @@ function shadow(e){
 
   x = x + e.target.offsetLeft;
   y = y + e.target.offsetTop;
+
+  if(list.contains(e.target)){
+    x = x + list.offsetLeft;
+    y = y + list.offsetTop;
+  }
 
   let xWalk = Math.round((x / width * walk) - (walk / 2));
   let yWalk = Math.round((y / height * walk) - (walk / 2));
