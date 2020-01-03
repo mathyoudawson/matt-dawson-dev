cfa61921df0f2791cf33421ae7fac4946e3b6f37
Matthew Dawson <matthewdawson@outlook.com>
2020-01-03 14:45:54 +1300

    Add a skew when mousing over attributes list

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 const body = document.querySelector('body');
-const list = document.querySelector('ul');
 const walk = 50;
+const attributesList = document.querySelector('.attributes');
 
 function shadow(e){
   const { offsetWidth: width, offsetHeight: height } = hero;
@@ -11,9 +11,9 @@ function shadow(e){
   x = x + e.target.offsetLeft;
   y = y + e.target.offsetTop;
 
-  if(list.contains(e.target) && e.target !== list){
-    x = x + list.offsetLeft;
-    y = y + list.offsetTop;
+  if(attributesList.contains(e.target) && e.target !== attributesList){
+    x = x + attributesList.offsetLeft;
+    y = y + attributesList.offsetTop;
   }
 
   let xWalk = Math.round((x / width * walk) - (walk / 2));
@@ -22,4 +22,16 @@ function shadow(e){
   text.style.textShadow = `${-xWalk}px ${-yWalk}px 5px #6791D1`;
 }
 
+function add_list_skew(){
+  attributesList.classList.add('skew-list');
+}
+
+function remove_list_skew(){
+  attributesList.classList.remove('skew-list');
+  attributesList.classList.add('fade-skew');
+}
+
+attributesList.addEventListener('mouseenter', add_list_skew);
+attributesList.addEventListener('mouseleave', remove_list_skew);
 body.addEventListener('mousemove', shadow);
+
